62c63107baaaef3505b4ede25207e4456a5ebb24
mj-kiran <kiranmjoshua@gmail.com>
2023-10-06 17:28:14 +0530

    changes updated

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+ mongoose
+   .connect(process.env.MONGODB_URL)
+   .then(() => {
+     console.log("Connected to MongoDB");
+   })
+   .catch((err) => {
+     console.error("Error", err);
+   });
 
 const app = express()
 
