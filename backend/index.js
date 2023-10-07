8850e3da6bae6adcc3380175040edaed41ac2ff8
mj-kiran <kiranmjoshua@gmail.com>
2023-10-07 20:55:17 +0530

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import categoryRouter from './routes/categoryRoutes.js'
 import subcategoryRouter from './routes/subcategoryRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ dotenv.config()
      console.error("Error", err);
    });
 
+   const __dirname=path.resolve()
 const app = express()
 
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -26,6 +28,11 @@ app.use("/api/categories", categoryRouter);
 app.use("/api/categories/subcategory", subcategoryRouter);
 
 app.use("/api/products", productRouter); 
+
+app.use(express.static(path.join(__dirname, 'frontend','build','index.html')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'/frontend/build/'))
+})
 
 app.listen(3300, () => {
     console.log('server started at 3300');
